d2ff211ce251a969953307bdf785365715c4f619
Marta Pantaleeva <marta@talkamatic.se>
2020-05-11 12:43:59 +0200

    Entity: Change names of properties
    This change changes "semantic_name" to just "name", and
    "surface_form" to "natural_language_form" in order to comply with the
    naming of these in the entities in the semantic inputs in TDM's HTTP
    API for frontends.
    
    Change-Id: I1ed5decfc4c690763d76c4eb799bfd28124e3620

--- tala/model/entity.py
+++ tala/model/entity.py
@@ -6,31 +6,31 @@ class UnexpectedModalityException(Exception):
 
 
 class Entity(EqualityMixin):
-    def __init__(self, semantic_name: str, sort: str, surface_form: str, ddd_name: str = None) -> None:
-        self._semantic_name = semantic_name
+    def __init__(self, name: str, sort: str, natural_language_form: str, ddd_name: str = None) -> None:
+        self._name = name
         self._sort = sort
-        self._surface_form = surface_form
+        self._natural_language_form = natural_language_form
         self._ddd_name = ddd_name
 
     @property
-    def semantic_name(self) -> str:
-        return self._semantic_name
+    def name(self) -> str:
+        return self._name
 
     @property
     def sort(self) -> str:
         return self._sort
 
     @property
-    def surface_form(self) -> str:
-        return self._surface_form
+    def natural_language_form(self) -> str:
+        return self._natural_language_form
 
     @property
     def ddd_name(self) -> str:
         return self._ddd_name
 
     def __str__(self):
-        return "{}({}, {}, {}, {})".format(self.__class__.__name__, self._semantic_name, self._sort,
-                                           self._surface_form, self._ddd_name)
+        return "{}({}, {}, {}, {})".format(self.__class__.__name__, self._name, self._sort,
+                                           self._natural_language_form, self._ddd_name)
 
     def __repr__(self):
         return str(self)
