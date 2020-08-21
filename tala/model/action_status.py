7ce7bae00fa502538f794da44a8a95e8f8043135
Alex Berman <alex@talkamatic.se>
2020-08-21 14:39:07 +0200

    Epic #1773 Add Done.as_semantic_expression()
    Change-Id: I18bedd990aa4ab856b64fd714852760f84999ed8

--- tala/model/action_status.py
+++ tala/model/action_status.py
@@ -1,3 +1,9 @@
-class Done(object):
+from tala.model.semantic_object import SemanticObject
+
+
+class Done(SemanticObject):
     def __eq__(self, other):
         return isinstance(other, Done)
+
+    def as_semantic_expression(self):
+        return "done"
