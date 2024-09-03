ec102ac6bf4be0303b9afc3f910340b77bb610ef
Fredrik Kronlid <fredrik@talkamatic.se>
2024-09-03 19:53:28 +0200

    Updating MQTTClient
    Updates in order to cater for more needs from streaming sources.
    
     - if a request_id is set, also stream to request-specific channel
     - make it possible to stream persona, voice and chunks separately
     -
    
    Change-Id: Id86578f8d76e5be514c88a01c80505320d8c80e2

--- tala/utils/mqtt_client.py
+++ tala/utils/mqtt_client.py
@@ -15,6 +15,7 @@ class MQTTClient:
         self._port = int(port)
         self._connected = threading.Event()
         self._session_id = None
+        self._request_id = None
         self._client_id = client_id
 
         self._client = mqtt.Client(
@@ -37,22 +38,42 @@ class MQTTClient:
     def session_id(self):
         return self._session_id
 
-    @property
-    def streamed(self):
-        return self._streamed
-
     @session_id.setter
     def session_id(self, session_id):
         self._session_id = session_id
 
     @property
-    def topic(self):
-        return 'tm/id/' + self.session_id
+    def request_id(self):
+        return self._request_id
 
-    def stream_utterance(self, utterance):
-        event = {"event": "STREAMING_CHUNK", "data": utterance}
-        self._stream_to_frontend(event)
-        self._streamed.append(utterance)
+    @request_id.setter
+    def request_id(self, request_id):
+        self._request_id = request_id
+
+    @property
+    def streamed(self):
+        return self._streamed
+
+    @property
+    def topics(self):
+        yield f'tm/id/{self.session_id}'
+        if self.request_id:
+            yield f'tm/id/{self.session_id}-{self.request_id}'
+
+    def stream_utterance(self, persona=None, voice=None, utterance="", status=None):
+        self.set_persona(persona)
+        self.set_voice(voice)
+        self.stream_chunk(utterance + " ")
+
+    def set_persona(self, persona):
+        self._stream_to_frontend({"event": "STREAMING_SET_PERSONA", "data": persona if persona else ""})
+
+    def set_voice(self, voice):
+        self._stream_to_frontend({"event": "STREAMING_SET_VOICE", "data": voice if voice else ""})
+
+    def stream_chunk(self, chunk):
+        self._stream_to_frontend({"event": "STREAMING_CHUNK", "data": chunk})
+        self._streamed.append(chunk)
 
     def prepare_stream(self):
         self._message_counter = 0
@@ -66,4 +87,5 @@ class MQTTClient:
         message |= {"id": f"{self._message_counter}_{self._client_id}"}
         self.logger.debug("streaming to frontend", message=message, session_id=self.session_id)
         self._connected.wait()
-        self._client.publish(self.topic, json.dumps(message))
+        for topic in self.topics:
+            self._client.publish(topic, json.dumps(message))
