908b06c0c9fd11a2a184786e2ed510d0e27f4eda
José M Cano Santín <jose@talkamatic.se>
2024-09-24 16:44:03 +0200

    Add create_entity to different TableHandler classes
    Change-Id: Ied448f3a285c4c55754835659dbe036782523aca

--- tala/utils/user_rates.py
+++ tala/utils/user_rates.py
@@ -14,8 +14,8 @@ class AbstractTableHandler():
             raise NoConnectionToDB("Table Service Client could not be reached.")
         self._table_client = table_service.get_table_client(table_name="UserRates")
 
-    def create_entity(self, offer_id: str, user_id: str):
-        self._table_client.create_entity(self._make_new_entity(offer_id, user_id))
+    def create_entity(self, *args):
+        raise NotImplementedError("Needs to be implemented in subclass %s" % self.__class__.__name__)
 
     def increment_num_calls(self, *args):
         raise NotImplementedError("Needs to be implemented in subclass %s" % self.__class__.__name__)
@@ -31,14 +31,8 @@ class AbstractTableHandler():
             entity[key] += 1
             self._table_client.update_entity(entity)
 
-    def _make_new_entity(self, offer_id: str, user_id: str):
-        return {
-            "PartitionKey": self.partition_key,
-            "RowKey": str(uuid.uuid4()),
-            "NumCalls": 0,
-            "OfferID": offer_id,
-            "UserID": user_id
-        }
+    def _make_new_entity(self, *args):
+        raise NotImplementedError("Needs to be implemented in subclass %s" % self.__class__.__name__)
 
     def _query_entities(self, key: str, value: str):
         filters = f"PartitionKey eq '{self.partition_key}' and {key} eq '{value}'"
@@ -48,14 +42,32 @@ class AbstractTableHandler():
 class HandlerUserRates(AbstractTableHandler):
     partition_key = "HandlerData"
 
+    def create_entity(self, offer_id: str, user_id: str):
+        self._table_client.create_entity(self._make_new_entity(offer_id, user_id))
+
     def increment_num_calls(self, offer_id: str):
         entities = self.query_offer_id(offer_id)
         self._increment_calls("NumCalls", entities[0])
+
+    def _make_new_entity(self, offer_id: str, user_id: str):
+        return {
+            "PartitionKey": self.partition_key,
+            "RowKey": str(uuid.uuid4()),
+            "NumCalls": 0,
+            "OfferID": offer_id,
+            "UserID": user_id
+        }
 
 
 class BuddyGeneratorUserRates(AbstractTableHandler):
     partition_key = "BuddyGeneratorData"
 
+    def create_entity(self, user_id: str):
+        self._table_client.create_entity(self._make_new_entity(user_id))
+
     def increment_num_calls(self, user_id: str):
         entities = self.query_user_id(user_id)
         self._increment_calls("NumCalls", entities[0])
+
+    def _make_new_entity(self, user_id: str):
+        return {"PartitionKey": self.partition_key, "RowKey": str(uuid.uuid4()), "NumCalls": 0, "UserID": user_id}
