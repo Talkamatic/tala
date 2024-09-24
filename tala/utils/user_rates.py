360162fddc8fce2d23ab8766c2c1fd317c32c313
José M Cano Santín <jose@talkamatic.se>
2024-09-24 11:00:17 +0200

    Task 905: Handle partition "BuddyGeneratorData"
    This change adds the handling of the table partition
    "BuddyGeneratorData" in the DB "UserRates".
    
    As a part of this change, the class "AbstractTableHandler" is
    refactored to handle the different partitions as different subclasses.
    
    Change-Id: I08faa0e1e094d0c7520195b46cfb4512243dd870

--- tala/utils/user_rates.py
+++ tala/utils/user_rates.py
@@ -6,43 +6,49 @@ class NoConnectionToDB(Exception):
     pass
 
 
-class TableHandler():
-    def __init__(self, connection_str):
+class AbstractTableHandler():
+    def __init__(self, connection_str: str):
         try:
             table_service = TableServiceClient.from_connection_string(conn_str=connection_str)
         except Exception:
             raise NoConnectionToDB("Table Service Client could not be reached.")
         self._table_client = table_service.get_table_client(table_name="UserRates")
 
+    def create_entity(self, offer_id: str, user_id: str):
+        self._table_client.create_entity(self._make_new_entity(offer_id, user_id))
+
+    def increment_num_calls(self, offer_id: str):
+        entities = self.query_offer_id(offer_id)
+        self._increment_calls("NumCalls", entities[0])
+
     def query_user_id(self, user_id: str):
-        return self._query_entities_in_handler_data("UserID", user_id)
+        return self._query_entities("UserID", user_id)
 
     def query_offer_id(self, offer_id: str):
-        return self._query_entities_in_handler_data("OfferID", offer_id)
-
-    def increment_num_calls_handler(self, offer_id: str):
-        entities = self.query_offer_id(offer_id)
-        self._increment_calls("NumCallsHandler", entities[0])
-
-    def increment_num_calls_buddy_generator(self, offer_id: str):
-        entities = self.query_offer_id(offer_id)
-        self._increment_calls("NumCallsBuddyGenerator", entities[0])
-
-    def create_entity(self, offer_id: str, user_id: str):
-        entity = {
-            "PartitionKey": "HandlerData",
-            "RowKey": str(uuid.uuid4()),
-            "NumCallsHandler": 0,
-            "OfferID": offer_id,
-            "UserID": user_id
-        }
-        return self._table_client.create_entity(entity)
+        return self._query_entities("OfferID", offer_id)
 
     def _increment_calls(self, key: str, entity: dict):
         if entity:
             entity[key] += 1
             self._table_client.update_entity(entity)
 
-    def _query_entities_in_handler_data(self, key: str, value: str):
-        filters = f"PartitionKey eq 'HandlerData' and {key} eq '{value}'"
+    def _make_new_entity(self, offer_id: str, user_id: str):
+        return {
+            "PartitionKey": self.partition_key,
+            "RowKey": str(uuid.uuid4()),
+            "NumCalls": 0,
+            "OfferID": offer_id,
+            "UserID": user_id
+        }
+
+    def _query_entities(self, key: str, value: str):
+        filters = f"PartitionKey eq '{self.partition_key}' and {key} eq '{value}'"
         return list(self._table_client.query_entities(filters))
+
+
+class HandlerUserRates(AbstractTableHandler):
+    partition_key = "HandlerData"
+
+
+class BuddyGeneratorUserRates(AbstractTableHandler):
+    partition_key = "BuddyGeneratorData"
