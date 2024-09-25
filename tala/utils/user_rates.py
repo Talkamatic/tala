eac52285e6d69cc85ec44f4238f0dad93ede202a
Fredrik Kronlid <fredrik@talkamatic.se>
2024-09-25 16:45:40 +0200

    Task 925: Track num calls last hour in HandlerUserRates
    Previously, the call rate handler in tala.utils.user_rates was not
    tracking the number of calls for a time unit. This change adds this
    kind of tracking, so that the database is keeping track of the number
    of calls to a certain offer in the last n minutes.
    
    Also:
    
      - Add unit tests for user_rates
    
    Change-Id: I327995c2d266c6d36673d2ab932be1548ac94943

--- tala/utils/user_rates.py
+++ tala/utils/user_rates.py
@@ -1,8 +1,17 @@
+from datetime import timedelta, datetime
 import uuid
+import time
+
 from azure.data.tables import TableServiceClient
+
+DEFAULT_AGE_LIMIT = 60
 
 
 class NoConnectionToDB(Exception):
+    pass
+
+
+class OfferNotInDB(Exception):
     pass
 
 
@@ -39,15 +48,56 @@ class HandlerUserRates(AbstractTableHandler):
     def create_entity_author_quota(self, user_id: str, author_quota: int):
         self._table_client.create_entity(self._make_new_entity_author_quota(user_id, author_quota))
 
-    def increment_num_calls(self, offer_id: str):
+    def query_offer_id_and_garbage_collect(self, offer_id: str, age_limit=DEFAULT_AGE_LIMIT):
+        def garbage_collect(entity, age_limit=DEFAULT_AGE_LIMIT):
+            to_remove = []
+            now = datetime.now()
+            for timestamp in entity.get("CallsLastPeriod", []):
+                if datetime.fromtimestamp(timestamp) + timedelta(minutes=age_limit) < now:
+                    to_remove.append(timestamp)
+                else:
+                    break
+            for item in to_remove:
+                entity["CallsLastPeriod"].remove(item)
+
         entities = self.query_offer_id(offer_id)
-        self._increment_calls("NumCalls", entities[0])
+        for entity in entities:
+            garbage_collect(entity, age_limit)
+            self._table_client.update_entity(entity)
+        return entities
+
+    def increment_num_calls(self, offer_id, age_limit=DEFAULT_AGE_LIMIT):
+        def increment_calls(entity):
+            if entity["NumCalls"] < 0:
+                pass
+            else:
+                entity["NumCalls"] += 1
+
+        def log_call_in_last_period(entity):
+            try:
+                entity["CallsLastPeriod"].append(time.time())
+            except KeyError:
+                entity["CallsLastPeriod"] = [time.time()]
+
+        def get_entry_for_offer_id(offer_id):
+            try:
+                return self.query_offer_id(offer_id)[0]
+            except IndexError:
+                raise OfferNotInDB(f"Offer {offer_id} was not found in DB")
+            except TypeError:
+                raise OfferNotInDB(f"Offer {offer_id} was not found in DB")
+
+        entity = get_entry_for_offer_id(offer_id)
+        increment_calls(entity)
+        log_call_in_last_period(entity)
+        self._table_client.update_entity(entity)
 
     def _make_new_entity(self, offer_id: str, user_id: str, offer_quota: int):
         return {
             "PartitionKey": self.partition_key,
             "RowKey": str(uuid.uuid4()),
             "NumCalls": 0,
+            "CallsLastPeriod": [],
             "OfferID": offer_id,
             "UserID": user_id,
             "OfferQuota": offer_quota
